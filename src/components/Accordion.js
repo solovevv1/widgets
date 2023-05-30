9827c972bb9915ef7d4186e8f92b1458bca50721
solovevv1 <solovevvs1@gmail.com>
2023-05-30 20:13:59 +0300

    useEffect, axios to wiki

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
 
 // Helper function for onClick event in the title
   const onTitleClick = (index) => {
-    console.log("Clicked", index);
+    // console.log("Clicked", index);
+    setActiveIndex(index);
   }
 
+// Destructure the items using the "map" method
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : ''
     return <React.Fragment key={item.title}>
-      <div className='title active'
+      <div className={`title + ${active}`}
         onClick={() => onTitleClick(index)}>
         <i className='dropdown icon'></i>{item.title}</div>
-      <div className='content active'>{item.content}</div>
+      <div className={'content' + active}>{item.content}</div>
     </React.Fragment>
   });
 
