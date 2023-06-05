9075435e068201880a3ebf649ebb6d0266b9c897
solovevv1 <solovevvs1@gmail.com>
2023-06-05 10:18:53 +0300

    the dropdwon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import Accordion from './components/Accordion'
 // import Search from './components/Search'
 import Dropdown from './components/Dropdown'
@@ -21,25 +21,30 @@ const items = [
 const options = [
   {
     label: 'The Color Red',
-    value: 'red'
+    value: 'red',
   },
   {
     label: 'The Color Green',
-    value: 'green'
+    value: 'green',
   },
   {
     label: 'The Shade of Blue',
-    value: 'blue'
+    value: 'blue',
   }
 ]
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
 
   return (
     <div>
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown options={options} />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   )
 }
