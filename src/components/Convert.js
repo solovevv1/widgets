c43fe0f15550121f6d835783d29e895fce94d0ef
solovevv1 <solovevvs1@gmail.com>
2023-06-06 11:57:04 +0300

    completed Convert component with delayed request

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,12 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const Convert = ({ language, text }) => {
+    const [translated, setTranslated] = useState(''); // to store translated text (piece of state)
+    const [debouncedText, setDebouncedText] = useState(text);
+
     useEffect(() => {
-        console.log("new language or text")
-    }, [language, text]); // anytime we get new language or text, we gonna run a function inside the useEffect 
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [text]);
+
+    useEffect(() => {
+        const doTranslation = async () => { // helper function, we can't use 'async'/'await' directly in useEffect
+            const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params: {
+                    q: debouncedText,
+                    target: language.value,
+                    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+                }
+            });
+            setTranslated(data.data.translations[0].translatedText);
+        };
+
+        doTranslation();
+    }, [language, debouncedText]); // anytime we get new language or text, we gonna run a function inside the useEffect 
 
     return (
-        <div>Convert</div>
+        <div>{translated}</div>
     )
 }
 
